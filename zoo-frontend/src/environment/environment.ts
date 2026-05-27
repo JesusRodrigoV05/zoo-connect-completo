a115659bb21991050bf064cc2f9cd02afb0750b9
JesusRodrigoV <jaicelvelasco105@gmail.com>
2026-05-26 22:31:25 -0400

    url back

--- zoo-frontend/src/environment/environment.ts
+++ zoo-frontend/src/environment/environment.ts
@@ -2,8 +2,9 @@ export const environment = {
   production: true,
   //marca: "ZooConnect",
   marca: "ZooConnect",
-  apiUrl: "http://localhost:8000/zooconnect",
-  //apiUrl: "https://zoo-connect-backend2.onrender.com/zooconnect",
+  //apiUrl: "http://localhost:8000/zooconnect",
+  apiUrl:"https://zoo-connect-completo.onrender.com/zooconnect",
+  //apiUrl: "https://zoo-connect-backend2.onrender.com/zooc
   // ReCAPTCHA v2 Site Key (visible widget) - Obtener en https://www.google.com/recaptcha/admin
   recaptchaSiteKey: "6LemotEsAAAAAAPdcpfXaAdb2PfK93wKdGZownBh",
 };
