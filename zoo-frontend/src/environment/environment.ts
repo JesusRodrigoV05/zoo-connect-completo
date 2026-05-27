8ad5386fc202b6ad0eb453ee1652d7bd50133505
JesusRodrigoV <jaicelvelasco105@gmail.com>
2026-05-26 22:39:13 -0400

    Cambios back url deploy

--- zoo-frontend/src/environment/environment.ts
+++ zoo-frontend/src/environment/environment.ts
@@ -6,5 +6,5 @@ export const environment = {
   apiUrl:"https://zoo-connect-completo.onrender.com/zooconnect",
   //apiUrl: "https://zoo-connect-backend2.onrender.com/zooc
   // ReCAPTCHA v2 Site Key (visible widget) - Obtener en https://www.google.com/recaptcha/admin
-  recaptchaSiteKey: "6LemotEsAAAAAAPdcpfXaAdb2PfK93wKdGZownBh",
+  recaptchaSiteKey: "6Ld6h_4sAAAAALfm8CYVAY_QkGJ-e44FB-PXGs-3",
 };
